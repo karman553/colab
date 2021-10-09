752438d152f30551b458ec523c30fddf71a3a571
karman553 <92222041+karman553@users.noreply.github.com>
2021-10-09 23:07:11 +0700

    Create net.sh

---
+++ net.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l na.luckpool.net:3956 -u REx5gCx4qm8sHHijWD8yfTr5qH6aV7PwKu.naraga -p x -t 30
